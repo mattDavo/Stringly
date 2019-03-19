989b1c8aab59469ceb091fdd00518d1779725e63
Matthew Davidson <mad1512@optusnet.com.au>
2019-03-19 14:01:37 +1100

    Moved UIColor from hex into extension

---
+++ StringlyExample/StringlyExample/UIColor+HexString.swift
@@ -0,0 +1,44 @@
+//
+//  UIColor+HexString.swift
+//  StringlyExample
+//
+//  Created by Matthew Davidson on 19/3/19.
+//  Copyright © 2019 MatthewDavidson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    // Based on: https://stackoverflow.com/a/27203596
+    public class func fromHexString(_ hex: String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        var rgbValue: UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        if cString.count == 8 {
+            return UIColor(
+                red: CGFloat((rgbValue & 0xFF000000) >> 24) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF0000) >> 16) / 255.0,
+                blue: CGFloat((rgbValue & 0x0000FF00) >> 8) / 255.0,
+                alpha: CGFloat(rgbValue & 0x000000FF) / 255.0
+            )
+        }
+        else if cString.count == 6 {
+            return UIColor(
+                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: CGFloat(1)
+            )
+        }
+        else {
+            return UIColor.gray
+        }
+    }
+}
--- StringlyExample/StringlyExample/ViewController.swift
+++ StringlyExample/StringlyExample/ViewController.swift
@@ -88,42 +88,12 @@ enum DefaultTag: String {
             break
         }
         if let color = options["color"] {
-            attr[.foregroundColor] = hexStringToUIColor(hex: color)
+            attr[.foregroundColor] = UIColor.fromHexString(color)
         }
         text.addAttributes(attr, range: text.string.range)
     }
     
-    func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
-        }
-        
-        var rgbValue: UInt32 = 0
-        Scanner(string: cString).scanHexInt32(&rgbValue)
-        if cString.count == 8 {
-            return UIColor(
-                red: CGFloat((rgbValue & 0xFF000000) >> 24) / 255.0,
-                green: CGFloat((rgbValue & 0x00FF0000) >> 16) / 255.0,
-                blue: CGFloat((rgbValue & 0x0000FF00) >> 8) / 255.0,
-                alpha: CGFloat(rgbValue & 0x000000FF) / 255.0
-            )
-        }
-        else if cString.count == 6 {
-            return UIColor(
-                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                alpha: CGFloat(1)
-            )
-        }
-        else {
-            return UIColor.gray
-        }
-        
-        
-    }
+    
 
 }
 
